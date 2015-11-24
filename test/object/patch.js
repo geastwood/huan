faf166279e64c1b29087c8d48b2fe483243965f5
Fei Liu <liu.feiwood@gmail.com>
2015-11-24 19:11:54 +0100

    add patch on object

--- test/object/patch.js
+++ test/object/patch.js
@@ -4,3 +4,19 @@ exports.patch = function(t) {
   t.deepEqual(obj.patch(['a', 'c'])({}), {a: undefined, c: undefined});
   t.done();
 };
+
+exports['patch-curry'] = function(t) {
+  t.deepEqual(
+    obj.patch(['b', 'c'])({b: 'b_1'}),
+    {b: 'b_1', c: undefined}
+  );
+  t.deepEqual(
+    obj.patch(['b', 'c', 'null'])({b: 'b_1'}),
+    {b: 'b_1', c: undefined, null: undefined}
+  );
+  t.deepEqual(
+    obj.patch(['b', 'c', 'null', 'new'])({b: 'b_1'}),
+    {b: 'b_1', c: undefined, null: undefined, 'new': undefined}
+  );
+  t.done();
+};
