30c2679f9be80d3d970ae4e497fd6fc9d2292cd7
Fei Liu <liu.feiwood@gmail.com>
2015-11-26 22:26:08 +0100

    prepare npm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import * as fp from './fp';
-import * as arr from './groupBy';
+import groupBy from './groupBy';
+import chunk from './chunk';
+import defaultTo from './defaultTo';
+import difference from './difference';
+import filterObj from './filterObj';
+import flatMap from './flatMap';
+import mapObj from './mapObj';
+import omit from './omit';
+import pair from './pair';
+import partition from './partition';
+import path from './patch';
+import pick from './pick';
+import pluck from './pluck';
+import project from './project';
+import reduceObj from './reduceObj';
+import valueIf from './valueIf';
+import values from './values';
 
 export default {
-  ...arr
+  ...fp,
+  groupBy,
+  chunk,
+  defaultTo,
+  difference,
+  filterObj,
+  flatMap,
+  mapObj,
+  omit,
+  pair,
+  partition,
+  path,
+  pick,
+  pluck,
+  project,
+  reduceObj,
+  valueIf,
+  values
 };
