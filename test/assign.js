a8c9b9a33189e70a57ae890bd0e44fe0c294680b
Fei Liu <liu.feiwood@gmail.com>
2016-08-12 17:55:42 +0200

    add a new test for assign

--- test/assign.js
+++ test/assign.js
@@ -4,12 +4,16 @@ var assign = huan.assign;
 exports.assign = t => {
   var deepEq = t.deepEqual;
   var c = {};
+
+  // not enumerable property is not copied
   Object.defineProperty(c, 'name', {
     value: 'c'
   });
+
   deepEq(assign({a: 'a'}, {b: 'b'}, {c: 'c'}), {a: 'a', b: 'b', c: 'c'});
   deepEq(assign({a: 'a'}, {}, {}), {a: 'a'});
   deepEq(assign({}, {a: 'a', b: 'b1'}, {a: 'a1'}, c), {a: 'a1', b: 'b1'});
   deepEq(assign({}, c), {});
+  deepEq(assign({}, {}), {});
   t.done();
 };
