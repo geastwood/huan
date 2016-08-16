83d01d7561552e1475d96986034390d5bf217cf2
Fei Liu <liu.feiwood@gmail.com>
2016-08-17 00:12:58 +0200

    add a test for reduceObj

--- test/reduceObj.js
+++ test/reduceObj.js
@@ -2,10 +2,12 @@ var huan = require('../lib')['default'];
 var reduceObj = huan.reduceObj;
 
 exports.reduceObj = t => {
-  t.deepEqual(reduceObj((carry, o) => carry.concat(o.value))([])({a: 'a1'}), ['a1']);
+  var de = t.deepEqual;
+
+  de(reduceObj((carry, o) => carry.concat(o.value))([])({a: 'a1'}), ['a1']);
 
   // to Paris
-  t.deepEqual(
+  de(
     reduceObj((carry, o) => carry.concat([[o.key, o.value]]))([])({a: 'a1', b: 'b1'}),
     [['a', 'a1'], ['b', 'b1']]
   );
