2303a1e637c4e16627728784c6f9ea2312267df6
Fei Liu <liu.feiwood@gmail.com>
2016-08-16 23:59:45 +0200

    refactor partition

--- src/partition.js
+++ src/partition.js
@@ -1,4 +1,5 @@
 import curry from './core/curry2';
+import {reduce} from './fp';
 
 /**
  * split xs into two groups which are packed into an array
@@ -10,8 +11,8 @@ import curry from './core/curry2';
  * (a -> Bool) -> [a] -> [[a], [a]]
  */
 export default curry((f, xs) => {
-  return xs.reduce((carry, v) => {
+  return reduce((carry, v) => {
     ((true === f(v)) ? carry[0] : carry[1]).push(v);
     return carry;
-  }, [[], []]);
+  }, [[], []], xs);
 });
